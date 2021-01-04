3fab91b879272fc542ef838fd36f9e84e164b98a
Grzegorz Kocoł <gb.kocol@gmail.com>
2021-01-04 13:51:13 +0100

    Initial commit

---
+++ Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,31 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Infrastructure.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+              => options.UseSqlite("Data Source=Skinet.db");
+
+        public DbSet<Product> Products { get; set; }
+
+    }
+
+    //public class StoreFactory : IDesignTimeDbContextFactory<StoreContext>
+    //{
+    //    public StoreContext CreateDbContext(string[] args)
+    //    {
+
+    //        var optionsBuilder = new DbContextOptionsBuilder<StoreContext>();
+    //        optionsBuilder.UseSqlite("your connection string");
+
+    //        return new StoreContext(optionsBuilder.Options);
+    //    }
+    //}
+}
