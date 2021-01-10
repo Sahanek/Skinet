f8ed643ff31bb3e5e4eac757b23001a4294cc6c9
Grzegorz Kocoł <gb.kocol@gmail.com>
2021-01-10 18:59:22 +0100

    StaticFiles, Mapper, Specifications Pattern, Generic Repo Pattern

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,3 +1,5 @@
+using API.Helpers;
+using AutoMapper;
 using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Builder;
@@ -27,7 +29,10 @@ namespace API
             services.AddControllers();
             services.AddDbContext<StoreContext>(x =>
                 x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
+
             services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            services.AddAutoMapper(typeof(MappingProfiles));
 
             services.AddSwaggerGen(c =>
             {
@@ -48,6 +53,7 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+            app.UseStaticFiles();
 
             app.UseAuthorization();
 
---
+++ Core/Specifications/ProductsWithTypesAndBrandsSpecifications.cs
@@ -0,0 +1,26 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Specifications
+{
+    public class ProductsWithTypesAndBrandsSpecifications : BaseSpecification<Product>
+    {
+        public ProductsWithTypesAndBrandsSpecifications()
+        {
+            AddInclude(p => p.ProductType);
+            AddInclude(p => p.ProductBrand);
+        }
+
+        public ProductsWithTypesAndBrandsSpecifications(int id) :
+            base(x => x.Id == id)
+        {
+            AddInclude(p => p.ProductType);
+            AddInclude(p => p.ProductBrand);
+        }
+    }
+}
