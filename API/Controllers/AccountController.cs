008917d23e9e2e505c1c5666dc3e113385a4f612
Grzegorz Kocoł <gb.kocol@gmail.com>
2021-02-07 19:35:36 +0100

    Validation

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -44,7 +44,7 @@ namespace API.Controllers
         }
 
         [HttpGet("emailexists")]
-        public async Task<ActionResult<bool>> CheckEmailExistsAsyn([FromQuery] string email)
+        public async Task<ActionResult<bool>> CheckEmailExistsAsync([FromQuery] string email)
         {
             return await _userManager.FindByEmailAsync(email) != null;
         }
@@ -95,6 +95,15 @@ namespace API.Controllers
         [HttpPost("register")]
         public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
         {
+            if (CheckEmailExistsAsync(registerDto.Email).Result.Value)
+            {
+                return new BadRequestObjectResult(new ApiValidationErrorResponse
+                {
+                    Errors = new[]
+                    {"Email address is in use"}
+                });
+            }
+
             var user = new AppUser
             {
                 DisplayName = registerDto.DisplayName,
